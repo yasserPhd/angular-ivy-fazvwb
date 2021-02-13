e01b62f057feb34c846fb23a420368f38d3e35ca
yasserPhd <oussalah.yasser@hotmail.fr>
2021-02-13 17:46:15 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { gymnastComponent} from './gymnast.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {Routes} from '@angular/router';
+
+import { addAthlete} from './addAthlete';
+
+const appRoutes: Routes=[{path:'addAthlete',component:addAthlete}]
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
